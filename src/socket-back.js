a63e60a09ba9720aa177b8966a63d9abac4af5de
ChristalCamillo <christal.b.camillo@gmail.com>
2024-02-16 12:29:35 -0300

    emitindo eventos

--- src/socket-back.js
+++ src/socket-back.js
@@ -3,4 +3,9 @@ import io from "./servidor.js";
 
 io.on('connection', (socket) => {
     console.log('a user connected! ID:', socket.id);
-  });
+
+
+    socket.on("texto_editor", (texto) => {
+        console.log(texto);
+    })
+});

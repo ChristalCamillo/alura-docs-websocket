ed23b36b3da8cedb1f380e7cdf44641c8090ef5f
ChristalCamillo <christal.b.camillo@gmail.com>
2024-02-16 16:30:15 -0300

    criando salas com socket.join

--- public/socket-front-documento.js
+++ public/socket-front-documento.js
@@ -2,6 +2,15 @@ import { atualizaTextoEditor } from "./documento.js";
 
 const socket = io();
 
+function selecionarDocumento(nome) {
+    socket.emit("selecionar_documento", nome);
+};
+
+socket.on("disconnect", (motivo) => {
+    console.log(`Servidor desconectado!
+    Motivo: ${motivo}`);
+  });
+
 function emitirTextoEditor(texto) {
     socket.emit("texto_editor", texto);
 };
@@ -10,4 +19,4 @@ socket.on("texto_editor_clientes", (texto) => {
     atualizaTextoEditor(texto);
 });
 
-export {emitirTextoEditor};
+export {emitirTextoEditor, selecionarDocumento};
--- src/socket-back.js
+++ src/socket-back.js
@@ -4,6 +4,14 @@ import io from "./servidor.js";
 io.on('connection', (socket) => {
     console.log('a user connected! ID:', socket.id);
 
+    socket.on("selecionar_documento", (nomeDocumento) => {
+        socket.join(nomeDocumento);
+    });
+
+    socket.on("disconnect", (motivo) => {
+        console.log(`Cliente "${socket.id}" desconectado!
+        Motivo: ${motivo}`);
+      });
 
     socket.on("texto_editor", (texto) => {
         socket.broadcast.emit("texto_editor_clientes", texto);

eaaf14959ad993e5b505d2c08d364dcadc537e8b
ChristalCamillo <christal.b.camillo@gmail.com>
2024-02-19 17:37:42 -0300

    guardando os dados localmente

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,10 +1,29 @@
 import io from "./servidor.js";
 
+const documentos = [
+    {
+        nome: "JavaScript",
+        texto: "texto de javascript"
+    },
+    {
+        nome: "Node",
+        texto: "texto de node"
+    },
+    {
+        nome: "Socket.IO",
+        texto: "texto de socket"
+    }
+];
+
 
 io.on('connection', (socket) => {
     console.log('a user connected! ID:', socket.id);
 
+    //quando o evento selecionar documento acontecer quero buscar o texto do doc e enviar de volta para o cliente
     socket.on("selecionar_documento", (nomeDocumento) => {
+        const documento = encontrarDocumento(nomeDocumento);
+
+
         socket.join(nomeDocumento);
     });
 
@@ -18,3 +37,11 @@ io.on('connection', (socket) => {
         socket.to(nomeDocumento).emit("texto_editor_clientes", texto);
     });
 });
+
+function encontrarDocumento(nome) {
+    const documento = documentos.find((documento) => {
+        return documento.nome === nome;
+    });
+
+    return documento;
+};
